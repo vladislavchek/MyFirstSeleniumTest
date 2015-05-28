45b80bf232a175f8cd1ad709d176be8424677c8b
Vladyslav Yeshchenko <vladyslav.yeshchenko@gmail.com>
2015-05-28 21:31:23 +0300

    first commit

---
+++ src/test/java/RozetkaTest.java
@@ -0,0 +1,21 @@
+package com.eviltester.webdriver;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstTest {
+
+    @Test
+    public void startWebDriver(){
+
+        WebDriver driver = new FirefoxDriver();
+
+        driver.navigate().to("http://rozetka.com.com");
+
+        Assert.assertTrue("title should start differently",
+                driver.getTitle().startsWith("Selenium Simplified"));
+
+        driver.close();
+        driver.quit();
